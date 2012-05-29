35faacf8fc2b37be0887f18f1be09e9631acb191
Florian Friesdorf <flo@chaoflow.net>
2012-05-29 22:02:03 +0200

    _next.all fails currently, if the real _next is missing

--- src/metachao/tests/test_next_all.py
+++ src/metachao/tests/test_next_all.py
@@ -21,5 +21,11 @@ class AllMethodsViaNext(TestCase):
             def g(_next, self):
                 return _next() + _next.all.f()
 
+            # @aspect.plumb
+            # def h(_next, self):
+            #     return _next() + _next.all.f()
+
         self.assertEqual(a(C()).g(), 3)
         self.assertEqual(a(C)().g(), 3)
+        # self.assertEqual(a(C()).h(), 3)
+        # self.assertEqual(a(C)().h(), 3)
