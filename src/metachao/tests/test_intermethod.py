21925d962d21a8ccaf454d374c2b34193b73dca3
Florian Friesdorf <flo@chaoflow.net>
2012-06-24 18:59:04 +0200

    attempt to make test more readable

--- src/metachao/tests/test_intermethod.py
+++ src/metachao/tests/test_intermethod.py
@@ -15,6 +15,7 @@ class InterMethod(TestCase):
 
             def g(self):
                 return self.f() * 2
+        c = C()
 
         self.assertEqual(C().f(), 1)
         self.assertEqual(C().g(), 2)
@@ -25,9 +26,9 @@ class InterMethod(TestCase):
 
         self.assertEqual(a1(C)().f(), 10)
         self.assertEqual(a1(C)().g(), 20)
-        self.assertEqual(a1(C()).f(), 10)
+        self.assertEqual(a1(c).f(), 10)
 
         # ATTENTION: C.f is picked instead of a1.f, because g is bound
         # to the C instance, not the new instance that uses the C
         # instance as prototype
-        self.assertEqual(a1(C()).g(), 2)
+        self.assertEqual(a1(c).g(), 2)
