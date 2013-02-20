2648ad46360c43a2705b9760bd258f5f2c06f2ce
Florian Friesdorf <flo@chaoflow.net>
2013-02-20 20:52:33 +0100

    no interact - does not install right now

--- src/metachao/tests/test_doctests.py
+++ src/metachao/tests/test_doctests.py
@@ -1,7 +1,6 @@
 import unittest
 import doctest
 from pprint import pprint
-from interlude import interact
 
 optionflags = doctest.NORMALIZE_WHITESPACE | \
               doctest.ELLIPSIS | \
@@ -27,8 +26,7 @@ def test_suite():
         doctest.DocFileSuite(
             file,
             optionflags=optionflags,
-            globs={'interact': interact,
-                   'pprint': pprint},
+            globs={'pprint': pprint},
             ) for file in TESTFILES
         ])
 
