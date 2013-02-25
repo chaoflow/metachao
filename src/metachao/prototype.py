84b751ea98a540eaca40570c2e768b1c62184ff6
Florian Friesdorf <flo@chaoflow.net>
2013-02-25 09:32:33 +0100

    greatly simplify by binding methods on gettatr (vs derivation)

--- src/metachao/prototype.py
+++ src/metachao/prototype.py
@@ -18,51 +18,28 @@ class boundproperty(property):
 
 def prototyper__getattr__(self, name):
     attr = getattr(self.__metachao_prototype__, name)
-    if callable(attr) and not name in self.__metachao_bound_methods__:
-        attr = attr.im_func.__get__(self, self.__class__)
+    if callable(attr):
+        bindto = getattr(self, '__metachao_bind_%s__' % (name,), None)
+        if bindto is None:
+            bindto = self
+        attr = attr.im_func.__get__(bindto, bindto.__class__)
     return attr
 
 
 def derive(prototype, bind=None):
-    bound_methods =  set(
-        getattr(prototype, '__metachao_bound_methods__', set())
-        )
     dct = {
         '__getattr__': prototyper__getattr__,
-        '__metachao_bound_methods__': bound_methods,
         '__metachao_prototype__': prototype,
         }
 
-    # all these need to be bound, once we are done
-    to_bind = bind is not None and bind.keys() or []
+    for name in bind or ():
+        dct['__metachao_bind_%s__' % name] = bind[name]
 
     for name, attr in getmembers(prototype.__class__):
-        # properties need to be put in our class' dict anyway,
-        # independent of whether bound or not
         if isinstance(attr, property):
             if not isinstance(attr, boundproperty):
                 attr = boundproperty(attr, name)
-            if name in to_bind:
-                slot = '__metachao_bind_%s__' % name
-                dct[slot] = bind[name]
-                to_bind.remove(name)
             dct[name] = attr
-        # methods are only put into our class' dict, if they are to be
-        # bound to something else than our instance
-        elif name in to_bind:
-            attr = attr.__get__(bind[name], bind[name].__class__)
-            dct[name] = attr
-            to_bind.remove(name)
-            bound_methods.add(name)
-
-    # search for the remaining methods to be bound. properties can't
-    # be left now, because they would have been found via the
-    # immediate prototype above
-    for name in to_bind:
-        attr = getattr(prototype, name)
-        attr = attr.im_func.__get__(bind[name], bind[name].__class__)
-        dct[name] = attr
-        bound_methods.add(name)
 
     name = "Prototyper"
     bases = ()
