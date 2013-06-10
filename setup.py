8d796b563069f3545069874c2783bbc825c0e6bf
Alexander Pilz <pilz@syslab.com>
2013-06-10 15:08:58 +0200

    Preparing release 0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0dev'
+version = '0.5'
 shortdesc = ""
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
