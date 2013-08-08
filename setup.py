5690d1544e979862c308dea82442342606c40edf
Alexander Pilz <pilz@syslab.com>
2013-08-08 12:26:42 +0200

    Back to development: 0.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.7'
+version = '0.8.dev0'
 shortdesc = ""
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
