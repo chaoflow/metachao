8b311ad4ce22d80a74baea73a886652ae572af65
Jonas Hoersch <coroa@online.de>
2013-11-15 21:33:49 +0100

    classtree: add instantiate aspect
    the instantiate aspect is similar to the instantiate_upon_traversal
    aspect, but differs in two key aspects:
    1. classes set in the classtree supersede the original dictionary
    2. instead of just classes a node in the classtree can also consist of a
    tuple of (class, *parameters), where parameters are attribute names.
    Their values on the instance are used to instantiate the child nodes.

--- src/metachao/classtree.py
+++ src/metachao/classtree.py
@@ -81,3 +81,56 @@ class Base(object):
 @instantiate_upon_traversal
 class Node(Base, OrderedDict):
     pass
+
+
+class instantiate(aspect.Aspect):
+    """Self-instantiating class trees of dictionary-like nodes
+
+When used on classes with metaclass `node`, it takes care of using the
+tree of classes on the class and returns their on-demand created
+instances.
+
+saves the created instances in a dictionary in CLASSTREE_ATTR on the
+instance.
+    """
+    # __metaclass__ = node
+    @aspect.plumb
+    def __init__(_next, self, *args, **kwargs):
+        setattr(self, CLASSTREE_ATTR, dict())
+        _next(*args, **kwargs)
+
+    @aspect.plumb
+    def __getitem__(_next, self, key):
+        """Return an instance if it has been set in the classtree.
+
+Instances are saved in the CLASSTREE_ATTR on the instance. Returns
+either
+1. an already created instance from this attribute
+2. a new instance if a class has been provided with the classtree
+3. the result of __getitem__ on the original class
+
+instead of just a classname, the classtree can also hold a tuple,
+where the first item is a class name and the rest are attributes of
+the instance, which are used as parameters for creating the instance.
+        """
+
+        try:
+            return getattr(self, CLASSTREE_ATTR)[key]
+        except KeyError:
+            pass
+
+        try:
+            cls = self.__class__[key]
+            if isinstance(cls, tuple):
+                params = (getattr(self, p) for p in cls[1:])
+                cls = cls[0]
+            else:
+                params = ()
+
+            node = cls(*params)
+            getattr(self, CLASSTREE_ATTR)[key] = node
+            return node
+        except KeyError:
+            pass
+
+        return _next(key)
